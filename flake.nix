7162366a5955b9f31b4c56f1280896d11538fe0d
Stanislovas Kujalis <stanislovas@pikta.lt>
2026-05-26 20:45:24 +0300

    trying to build dongles

--- flake.nix
+++ flake.nix
@@ -17,17 +17,13 @@
       # Common arguments shared across all pieces
       commonArgs = {
         src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
-        zephyrDepsHash = "sha256-cLXBrzTd5dy3LF8DNk0AsyHjaJmTVmmZtBklHccoUC0="; # Keep empty to force hash recalculation on your first run
+        zephyrDepsHash = "sha256-GUXpdxC7LrXw02Yxrdqzi9qrHFZdIcX44iGfGm3v9lM="; # Keep empty to force hash recalculation on your first run
         meta = {
           description = "ZMK firmware for Eyelash Sofle & Dongle";
           license = nixpkgs.lib.licenses.mit;
           platforms = nixpkgs.lib.platforms.all;
         };
       };
-
-      # ---------------------------------------------------------
-      # TARGET 1 & 2: THE KEYBOARD (What you will use right now)
-      # ---------------------------------------------------------
 
       # Left Half (Acts as Central brain by default)
       left = builder (commonArgs // {
@@ -43,32 +39,23 @@
         shield = "nice_view_adapter nice_view";
       });
 
-
-      # ---------------------------------------------------------
-      # TARGET 3 & 4: THE DONGLE GUESSES (For when it arrives)
-      # ---------------------------------------------------------
-      # We define Seller A's custom matrix shield, but map it to 
-      # the two microcontrollers Seller B most likely used.
-
       dongle-nano = builder (commonArgs // {
         name = "dongle-nano-firmware";
-        board = "eyelash_sofle_central_dongle";
-        shield = ""; 
+        board = "nice_nano_v2";
+        shield = "sweep_dongle dongle_display"; 
       });
 
       dongle-xiao = builder (commonArgs // {
         name = "dongle-xiao-firmware";
-        board = "seeeduino_xiao_ble";
-        shield = "eyelash_sofle_central_dongle"; 
+        board = "xiao_ble";
+        shield = "sweep_dongle dongle_display"; 
       });
-
-      # all of em
 
       all = nixpkgs.legacyPackages.${system}.linkFarm "all-firmwares" [
         { name = "left"; path = left; }
         { name = "right"; path = right; }
-        # { name = "dongle-nano"; path = dongle-nano; }
-        # { name = "dongle-xiao"; path = dongle-xiao; }
+        { name = "dongle-nano"; path = dongle-nano; }
+        { name = "dongle-xiao"; path = dongle-xiao; }
       ];
 
       # Set 'all' as the default package
